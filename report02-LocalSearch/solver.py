9a7349e8886dc411bab91884bab79f868b62535a
Nima Hosseini <nimahsn@hotmail.com>
2021-04-03 14:23:13 +0430

    logging added to solvers

--- report02-LocalSearch/solver.py
+++ report02-LocalSearch/solver.py
@@ -1,16 +1,33 @@
 from heapq import heapify, heappop, heappush, heapreplace
-from typing import Callable, List, Sequence
+from typing import Callable, List, Sequence, Tuple
 from problems import Problem
 from random import random
 import math
+import time
+
+class SolverLog:
+    def __init__(self) -> None:
+        self.depth = 0
+        self.nodes = 0
+        self.costs = []
+        self.elapsed_time = 0
+        self.temperatures=[]
+        
 
 class HillClimb:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, save_log = False) -> None:
+        self.save_log = save_log
 
-    def solve(self, problem: "Problem"):
+    def solve(self, problem: "Problem") -> "Problem":
+        if self.save_log:
+            log = SolverLog()
+            log.costs = [problem.get_cost()]
+            start_time_ns = time.time_ns()
         while True:
             if problem.is_goal():
+                if self.save_log:
+                    log.elapsed_time = time.time_ns() - start_time_ns
+                    self.log = log
                 return problem
             min_child = None
             min_cost = math.inf
@@ -20,24 +37,42 @@ class HillClimb:
                     min_child = child
                     min_cost = new_cost
             if min_cost < problem.get_cost():
+                if self.save_log:
+                    log.depth += 1
+                    log.costs.append(min_cost)
                 problem = min_child
             else:
+                if self.save_log:
+                    log.elapsed_time = time.time_ns() - start_time_ns
+                    self.log = log
                 return problem
                 
 
 class BeamSearch:
-    def __init__(self, beam: int) -> None:
+    def __init__(self, beam: int, save_log = False) -> None:
+        self.save_log = save_log
         self.k = beam
   
-    def solve(self, initial_states: List["Problem"]):
+    def solve(self, initial_states: List["Problem"]) -> "Problem":
+        if self.save_log:
+            log = SolverLog()
+            start_time_ns = time.time_ns()
         sequence = [None]*self.k
         for i,prob in enumerate(initial_states):
             sequence[i] = (prob.get_cost(), i, prob)
         entry = self.k        
         sequence.sort()
+        if self.save_log:
+            l=[]
+            for prob in sequence:
+                l.append(prob[0])
+            log.costs.append(l)
 
         while True:
             if sequence[0][2].is_goal():
+                if self.save_log:
+                    log.elapsed_time = time.time_ns() - start_time_ns
+                    self.log = log
                 return sequence[0][2]
             heap = []
             updated = False
@@ -50,27 +85,46 @@ class BeamSearch:
                         entry += 1
                         updated = True
             if not updated:
+                if self.save_log:
+                    log.elapsed_time = time.time_ns() - start_time_ns
+                    self.log = log
                 return sequence[0][2]
             for i in range(self.k):
                 sequence[i] = heappop(heap)
-            del heap
+            if self.save_log:
+                log.depth += 1
+                l = []
+                for problem in sequence:
+                    l.append(problem[0])
+                log.costs.append(l)
 
 class SimulatedAnnealing:
-    def __init__(self, min_temp: float = 1e-6, max_iter: int = 1000, scale_factor: int = 1.01) -> None:
+    def __init__(self, min_temp: float = 1e-6, max_iter: int = 1000, scale_factor: int = 1.01, save_log = False) -> None:
         self.max_iter = max_iter
         self.min_temp = min_temp
-        self.scale = scale_factor    
+        self.scale = scale_factor
+        self.save_log = save_log
 
     def solve(self, problem: Problem, init_temp: int):
+        if self.save_log:
+            log = SolverLog()
+            start_time_ns = time.time_ns()
         iter = 0
         for temp in self.scheduler(init_temp, self.scale):
             if problem.is_goal() or iter >= self.max_iter or temp < self.min_temp:
+                if self.save_log:
+                    log.elapsed_time = time.time_ns() - start_time_ns
+                    self.log = log
                 return problem
             # print("iter: {}, temp: {}, cost: {}".format(iter, temp, problem.get_cost()))
             iter += 1
             random_child = problem.get_random_child()
             new_cost = random_child.get_cost()
             curr_cost = problem.get_cost()
+            if self.save_log:
+                log.costs.append(curr_cost)
+                log.depth += 1
+                log.temperatures.append(temp)
             if new_cost < curr_cost:
                 problem = random_child
                 continue
