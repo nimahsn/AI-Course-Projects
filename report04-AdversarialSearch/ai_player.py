8e8f5a1be740f4b636dfc65b53a7e75fb4a355a5
Nima Hosseini <nimahsn@hotmail.com>
2021-04-10 20:32:21 +0430

    actions implemented in board

--- report04-AdversarialSearch/ai_player.py
+++ report04-AdversarialSearch/ai_player.py
@@ -1,5 +1,9 @@
 from board import State
 
 class PlayerAlphaBeta():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, black: bool) -> None:
+        self.black = black
+        self.markers_out = 9
+        self.markers_in = 0
+        self.markers_lost = 0
+        self.markers_captured = 0
--- report04-AdversarialSearch/board.py
+++ report04-AdversarialSearch/board.py
@@ -1,4 +1,6 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
+from copy import deepcopy
+from ai_player import PlayerAlphaBeta
 
 
 adjacent_dict = {
@@ -28,7 +30,34 @@ adjacent_dict = {
     (7,7):[(7,4), (4,7)]
 }
 
-positoins = [
+mill_dict = {
+    (1,1): [[(1,4), (1,7)], [(4,1), (7,1)]],
+    (1,4): [[(1,1), (1,7)], [(2,4), (3,4)]],
+    (1,7): [[(1,4), (1,1)], [(4,7), (7,7)]],
+    (2,2): [[(2,4), (2,6)], [(4,2), (6,2)]],
+    (2,4): [[(2,2), (2,6)], [(1,4), (3,4)]],
+    (2,6): [[(2,2), (2,4)], [(4,6), (6,6)]],
+    (3,3): [[(3,4), (3,5)], [(4,3), (5,4)]],
+    (3,4): [[(3,3), (3,5)], [(2,4), (1,4)]],
+    (3,5): [[(3,3), (3,4)], [(4,5), (5,5)]],
+    (4,1): [[(1,1), (7,1)], [(4,2), (4,3)]],
+    (4,2): [[(4,1), (4,3)], [(2,2), (6,2)]],
+    (4,3): [[(4,1), (4,2)], [(3,3), (5,3)]],
+    (4,5): [[(4,6), (4,7)], [(3,5), (5,5)]],
+    (4,6): [[(4,5), (4,7)], [(2,6), (6,6)]],
+    (4,7): [[(4,5), (4,6)], [(1,7), (7,7)]],
+    (5,3): [[(3,3), (4,3)], [(5,4), (5,6)]],
+    (5,4): [[(5,3), (5,5)], [(6,4), (7,4)]],
+    (5,5): [[(5,3), (5,4)], [(3,5), (4,5)]],
+    (6,2): [[(6,4), (6,6)], [(4,2), (2,2)]],
+    (6,4): [[(5,4), (7,4)], [(6,2), (6,6)]],
+    (6,6): [[(6,2), (6,4)], [(4,6), (2,6)]],
+    (7,1): [[(4,1), (1,1)], [(7,4), (7,7)]],
+    (7,4): [[(7,1), (7,7)], [(6,4), (5,4)]],
+    (7,7): [[(7,1), (7,4)], [(4,7), (1,7)]]
+}
+
+positions = [
     (1,1), (1,4), (1,7),
     (2,2), (2,4), (2,6),
     (3,3), (3,4), (3,5),
@@ -38,38 +67,160 @@ positoins = [
     (7,1), (7,4), (7,7)
 ]
 
+place_action = 1
+move_action = 2
+move_and_capture_action = 3
+fly_action = 4
+
+
 class Board:
-    def __init__(self, phase: int, black_markers: List["Marker"] = [], white_markers: List["Marker"] = []) -> None:
-        self.blacks: List["Marker"] = black_markers
-        self.whites: List["Marker"] = white_markers
+    def __init__(self, phase: int, dict_markers = {}) -> None:
+        self.pos_marker_dict: Dict[Tuple[int, int], "Marker"] = dict_markers
         self.phase = phase
+        self.black_markers_out = 9
+        self.white_marker_out = 9
+        self.black_markers_in = 0
+        self.white_markers_in = 0
+        self.white_markers_captured = 0
+        self.black_markers_captured = 0
     
-    def get_all_moves(self, black: bool):
+    def get_all_moves(self, player: "PlayerAlphaBeta"):
         if self.phase == 1:
-            for move in self.get_all_moves_phase1(black):
+            for move in self.get_all_moves_phase1(player):
                 yield move
         elif self.phase == 2:
-            for move in self.get_all_moves_phase2(black):
+            for move in self.get_all_moves_phase2(player):
                 yield move
         else:
-            for move in self.get_all_moves_phase3(black):
+            for move in self.get_all_moves_phase3(player):
                 yield move
 
-    def get_all_moves_phase1(self, black: bool):
-        pass
+    def get_all_moves_phase1(self, player):
+        for pos in positions:
+            if self.pos_marker_dict == None:
+                new_marker = Marker(black=player.black, position=pos)
+                # action = Action(place_action, marker=new_marker, new_pos=pos)
+                # yield action
+                board_cpy = deepcopy(self)
+                board_cpy.pos_marker_dict[pos] = new_marker
+                if player.black:
+                    self.black_markers_in += 1
+                    self.black_markers_out -= 1
+                else:
+                    self.white_marker_in += 1
+                    self.white_marker_out -= 1
+                if board_cpy.is_mill(pos, player.black):
+                    for b in board_cpy.remove_opp_marker(player):
+                        yield b
+                    continue
+                yield board_cpy
+        
+        
 
-    def get_all_moves_phase2(self, black: bool):
-        pass
+    def get_all_moves_phase2(self, player):
+        black = player.black
+        for pos in positions:
+            if self.pos_marker_dict[pos] is None:
+                continue
+            m = self.pos_marker_dict[pos]
+            if m.black != black:
+                continue
+            for new_pos in m.get_moves:
+                if self.pos_marker_dict[new_pos] is not None:
+                    continue
+                bcp = deepcopy(self)
+                new_m = Marker(black, new_pos)
+                bcp.pos_marker_dict[new_pos] = new_m
+                bcp.pos_marker_dict[pos] = None
+                if bcp.is_mill(pos, black):
+                    for b in bcp.remove_opp_marker(player):
+                        yield b
+                    continue
+                yield bcp
 
-    def get_all_moves_phase3(self, black: bool):
-        pass
+    def get_all_moves_phase3(self, player):
+        black = player.black
+        if black and self.black_markers_in > 3:
+            for b in self.get_all_moves_phase2(player):
+                yield b
+        elif not black and self.white_marker_in > 3:
+            for b in self.get_all_moves_phase2(player):
+                yield b
+        else:
+            for pos in positions:
+                if self.pos_marker_dict[pos] is None:
+                    continue
+                m = self.pos_marker_dict[pos]
+                if m.black != black:
+                    continue
+                for new_pos in positions:
+                    if self.pos_marker_dict[new_pos] is not None:
+                        continue
+                    bcp = deepcopy(self)
+                    new_m = Marker(black, new_pos)
+                    bcp.pos_marker_dict[new_pos] = new_m
+                    bcp.pos_marker_dict[pos] = None
+                    if bcp.is_mill(pos, black):
+                        for b in bcp.remove_opp_marker(player):
+                            yield b
+                        continue
+                    yield bcp
+                    
 
+    def is_mill(self, position, black) -> bool:
+        for l in mill_dict[position]:
+            if self.pos_marker_dict[l[0]].black == black and self.pos_marker_dict[l[1]].black == black:
+                return True
+        return False
+
+
+    def remove_opp_marker(self, player) -> "Board":
+        black = player.black
+        found = False
+        l=[]
+        for pos in positions:
+            m = self.pos_marker_dict[pos]
+            if m is None or m.black==black:
+                continue
+            if self.is_mill(pos, not black):
+                l.append(pos)
+                continue
+            found = True
+            bcp = deepcopy(self)
+            bcp.pos_marker_dict[pos] = None
+            if black:
+                bcp.white_marker_in -= 1
+                bcp.white_markers_captured +=1
+            else:
+                bcp.black_markers_in -= 1
+                bcp.black_markers_captured +=1
+            yield bcp
+        if found == False:
+            for pos in l:
+                bcp = deepcopy(self)
+                bcp.pos_marker_dict[pos] = None
+                if black:
+                    bcp.white_marker_in -= 1
+                    bcp.white_markers_captured +=1
+                else:
+                    bcp.black_markers_in -= 1
+                    bcp.black_markers_captured +=1
+                yield bcp
+
+
+
+class Action:
+    def __init__(self, type, marker, new_pos, capture: "Marker"=None) -> None:
+        self.type = type
+        self.marker = marker
+        self.new_pos = new_pos
+        self.capture_marker = capture
 class Marker:
-    def __init__(self, black: bool, position: Tuple[int, int], in_game: bool = False) -> None:
+    def __init__(self, black: bool, position: Tuple[int, int] = None, in_game: bool = True) -> None:
         self.black = black
         self.in_game = in_game
         self.position = position
     
     def get_moves(self):
-        for adj in adjacent_dict[positoins]:
+        for adj in adjacent_dict[self.position]:
             yield adj
