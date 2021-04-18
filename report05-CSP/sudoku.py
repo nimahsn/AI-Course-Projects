dfb11c2ffdaf37250bfeff77123c6b8bb8aebe61
Nima Hosseini <nimahsn@hotmail.com>
2021-04-18 16:46:21 +0430

    sudoku out_of_domain replaced with domain

--- report05-CSP/sudoku.py
+++ report05-CSP/sudoku.py
@@ -1,34 +1,33 @@
-from typing import List, Tuple
+from typing import Dict, List, Set, Tuple
 
 class Sudoku():
-    def __init__(self, init_board: List[List[int]]) -> None: #init_board must be a 9x9 list of integers. 
+    def __init__(self, init_board: List[List[int]], make_arc_consistency: bool = True) -> None: #init_board must be a 9x9 list of integers. 
         self.board = init_board
         self.neighbors = {}
         for i in range(9):
             for j in range(9):
-                self.neighbors[(i,j)] = [self.get_neighbors((i,j))]
+                self.neighbors[(i,j)] = list(self.get_neighbors((i,j)))
         self.variables = set()
         for i in range(9):
             for j in range(9):
                 if self.board[i][j] == 0:
                     self.variables.add((i,j))
-        self.out_of_domain_dict = {}
+        self.domain: Dict[Tuple[int, int], Set[int]] = {}
         for var in self.variables:
-            self.out_of_domain_dict[var] = []
-
-    def get_domain(self, index):
-        d = list(range(1,10))
-        for out in self.out_of_domain_dict[index]:
-            d.remove(out)
-        return d
-
-    def get_domain_size(self, index):
-        return 9 - len(self.out_of_domain_dict[index])
+            self.domain[var] = set(range(1, 10))
+        if make_arc_consistency:
+            for i in range(9):
+                for j in range(9):
+                    if self.board[i][j] != 0:
+                        for var in self.neighbors[(i,j)]:
+                            if var in self.variables and self.board[i][j] in self.domain[var]:
+                                self.domain[var].remove(self.board[i][j])
         
     def constraint_check(self, index, value) -> bool:
         for neighbor in self.neighbors[index]:
             if self.board[neighbor[0]][neighbor[1]] == value:
                 return False
+        return True
 
     @staticmethod
     def get_neighbors(index: Tuple[int, int]):
@@ -50,4 +49,10 @@ class Sudoku():
                 if j == index[1]:
                     continue
                 yield (i,j)
+    
+    def __repr__(self) -> str:
+        s = ""
+        for r in self.board:
+            s+=str(r) + "\n"
+        return s
     
